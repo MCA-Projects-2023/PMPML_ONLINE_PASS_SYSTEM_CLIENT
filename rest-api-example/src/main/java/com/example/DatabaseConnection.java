43f548645c534e7c01f0d8a846a4c290bf6dc132
Nagesh J <nageshjoshi2898@gmail.com>
2023-11-29 00:00:42 +0530

    added JDBC and abe to connect to DB as per API response

---
+++ rest-api-example/src/main/java/com/example/DatabaseConnection.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/pmpml";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "mysql";
+
+    private static Connection connection;
+
+    public static Connection connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+            System.out.println("Connected to the database.");
+            return connection;
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to connect to the database.");
+        }
+    }
+
+    public static void disconnect() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+                System.out.println("Disconnected from the database.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean isConnected() {
+        try {
+            return connection != null && !connection.isClosed();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
--- rest-api-example/src/main/java/com/example/Main.java
+++ rest-api-example/src/main/java/com/example/Main.java
@@ -1,13 +1,27 @@
 package com.example;
 
+import java.sql.Connection;
 import static spark.Spark.*;
 
 public class Main {
+
     public static void main(String[] args) {
+        Connection connection = DatabaseConnection.connect();
         port(8080); // Set the port (change as needed)
 
         get("/hello", (req, res) -> "Hello, Spark Java!"); // Define a simple endpoint
-
         // Add more endpoints as needed
+
+        get("/checkConnection", (req, res) -> {
+            if (DatabaseConnection.isConnected()) {
+                return "Connected to the database.";
+            } else {
+                return "Not connected to the database.";
+            }
+        });
+
+        // Ensure to disconnect from the database when the application exits
+        Runtime.getRuntime().addShutdownHook(new Thread(DatabaseConnection::disconnect));
+
     }
 }
